fe05922ae7002cc9b169e2ca1f403bc122ec74f1
auom.eisiad <daisiemouaa@gmail.com>
2024-01-16 14:25:14 -0500

    completed about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,78 +10,46 @@ import CardGroup from "react-bootstrap/CardGroup";
 // About function
 const About = () => (
   <CardGroup>
-      <Card className="about">
-        <Card.Body>
-          <Card.Title className="about-title">
-            Hi there! 
-          </Card.Title>
-          <Card.Text>
-            I'm Daisie, a passionate full-stack developer dedicated to crafting
-            user-friendly and accessible applications. I thrive in challenging
-            environments, navigating demanding client expectations, and excel
-            under pressure, meeting tight deadlines. Whether working
-            independently or collaboratively in a team, I bring enthusiasm and
-            expertise to every project. I look forward to creating something
-            amazing together!
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="skills">
-        <Card.Body>
-          <Card.Title className="skill-title">Skills:</Card.Title>
-          <Card.Text>
-            <ul className="skill-set">
-              <li>
-                HTML, CSS, JS
-              </li>
-              <li>
-                MySql
-              </li>
-              <li>
-                MongoDB
-              </li>
-              <li>
-                React.js
-              </li>
-              <li>
-                Bootstrap
-              </li>
-              <li>
-                jQuery
-              </li>
-              <li>
-                Express.js
-              </li>
-              <li>
-                Sequelize.js
-              </li>
-              <li>
-                Mongoose.js
-              </li>
-              <li>
-                MERN
-              </li>
-              <li>
-              Object-Oriented-Programming (OOP)
-              </li>
-              <li>
-              Application Programming Interfaces (API)
-              </li>
-              <li>
-              Command Line Interfaces (CLI)
-              </li>
-              <li>
-              Progressive Web Applications (PWA)
-              </li>
-              <li>
-              Functional-Programming (FP)
-              </li>
-              <li>
-                Git
-              </li>
-            </ul></Card.Text>
-        </Card.Body>
-      </Card>
+    <Card className="about">
+      <Card.Body>
+        <Card.Title className="about-title">Hi there! &#10022;</Card.Title>
+        <Card.Text>
+          I'm Daisie, a passionate full-stack developer dedicated to crafting
+          user-friendly and accessible applications. Each day I learn something
+          new and I strive for improvement. Whether working independently
+          or collaboratively in a team, I bring enthusiasm and expertise to
+          every project. I look forward to creating something amazing together!
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card className="skills">
+      <Card.Body>
+        <Card.Title className="skill-title">Skills:</Card.Title>
+        <Card.Text className="skill-sets">
+          <div className="skill-set1">
+            <div className="skill">HTML, CSS, JS</div>
+            <div className="skill">Git</div>
+            <div className="skill">MySql</div>
+            <div className="skill">MongoDB</div>
+            <div className="skill">React.js</div>
+          </div>
+          <div className="skill-set2">
+            <div className="skill">Bootstrap</div>
+            <div className="skill">jQuery</div>
+            <div className="skill">Express.js</div>
+            <div className="skill">Sequelize.js</div>
+            <div className="skill">Mongoose.js</div>
+          </div>
+          <div className="skill-set3">
+            <div className="skill">Object-Oriented-Programming (OOP)</div>
+            <div className="skill">Application Programming Interfaces (API)</div>
+            <div className="skill">Command Line Interfaces (CLI)</div>
+            <div className="skill">Progressive Web Applications (PWA)</div>
+            <div className="skill">Functional-Programming (FP)</div>
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   </CardGroup>
 );
 
