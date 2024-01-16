b322a480686b13dd832778020fa853015e341cc2
auom.eisiad <daisiemouaa@gmail.com>
2024-01-16 15:47:43 -0500

    added some extra stuff

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -12,53 +12,58 @@ const Project = () => {
       title: "Movie Reels",
       text: "Collaberative project building a movie watchlist website",
       imageUrl: "/images/movie-reels.png",
-      linkUrl: 'https://auom-eisiad.github.io/Movie-Reels/moviereels.html',
+      linkUrl: "https://auom-eisiad.github.io/Movie-Reels/moviereels.html",
       date: "August 28, 2023",
     },
     {
       title: "Lyrx",
       text: "Collabertive project building lyrics to their favorite song",
       imageUrl: "/images/lyrx.png",
-      linkUrl: 'https://lyrx-8c8792a2e8e6.herokuapp.com/',
+      linkUrl: "https://lyrx-8c8792a2e8e6.herokuapp.com/",
       date: "October 26, 2023",
     },
     {
       title: "5-day Weather Forecast",
       text: "Weather dashboard for today's weather and the next 5 days",
       imageUrl: "/images/weather-forecast.png",
-      linkUrl: 'https://auom-eisiad.github.io/06-Weather-Forecast/',
+      linkUrl: "https://auom-eisiad.github.io/06-Weather-Forecast/",
       date: "August 24, 2023",
     },
     {
       title: "Code Quiz",
       text: "A mini quiz to test your coding knowledge",
       imageUrl: "/images/code-quiz.png",
-      linkUrl: 'https://auom-eisiad.github.io/04-Code-Quiz/',
+      linkUrl: "https://auom-eisiad.github.io/04-Code-Quiz/",
       date: "August 10, 2023",
     },
     {
       title: "README Generator",
       text: "A professional README file generator",
       imageUrl: "/images/readme-generator.png",
-      linkUrl: 'https://github.com/auom-eisiad/07-README-Generator',
+      linkUrl: "https://github.com/auom-eisiad/07-README-Generator",
       date: "October 25, 2023",
     },
     {
       title: "Work Scheduler",
       text: "A to-do list for a 9-5 work schedule",
       imageUrl: "/images/workday-scheduler.png",
-      linkUrl: 'https://auom-eisiad.github.io/05-Work-Schedule/',
+      linkUrl: "https://auom-eisiad.github.io/05-Work-Schedule/",
       date: "August 18, 2023",
     },
   ];
 
   return (
+    <div className="project-container">
     <Row className="project-page">
       {cardsContent.map((card, idx) => (
         <Col className="procol" key={idx}>
           <Card className="project">
-          <a href={card.linkUrl}>
-            <Card.Img className="project-img" src={card.imageUrl} style={{ width: '80%'}} />
+            <a href={card.linkUrl} target="_blank" rel="noopener noreferrer">
+              <Card.Img
+                className="project-img"
+                src={card.imageUrl}
+                style={{ width: "80%" }}
+              />
             </a>
             <Card.Body>
               <Card.Title className="project-title">{card.title}</Card.Title>
@@ -71,7 +76,21 @@ const Project = () => {
         </Col>
       ))}
     </Row>
-  );
+    <Row className="extra">
+      <Col xs={12}>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            <a href="https://github.com/auom-eisiad" target="_blank" rel="noopener noreferrer">
+              <button className="github-btn">Check out my Github for more!</button>
+              </a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+);
 };
 
 export default Project;
