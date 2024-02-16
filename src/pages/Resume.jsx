efce488fd4d5616a2c633993324a64dfd703fee5
auom.eisiad <daisiemouaa@gmail.com>
2024-02-16 18:22:49 -0500

    added new resume url

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,7 +7,7 @@ import Button from "react-bootstrap/Button";
 // Resume function
 const Resume = () => {
     const downloadResume = () => {
-        const resumeUrl = 'public/daisie-moua-resume.pdf';
+        const resumeUrl = 'https://www.canva.com/design/DAF8Z0SWFb4/SiL8zH1tKI4SLMo-2j3aTw/view?utm_content=DAF8Z0SWFb4&utm_campaign=designshare&utm_medium=link&utm_source=editor';
         window.open(resumeUrl, '_blank');
       };
     
